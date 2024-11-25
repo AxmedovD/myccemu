857669faa64dc72ccc68f72f9db3d4b112b09e83
Macbook Pro <user@Davrons-Macbook-Pro.local>
2024-11-25 18:36:45 +0500

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Route::post('/login', [WebAuthController::class, 'login']);
 Route::get('/register', [WebAuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [WebAuthController::class, 'register']);
 Route::post('/logout', [WebAuthController::class, 'logout'])->name('logout');
+
